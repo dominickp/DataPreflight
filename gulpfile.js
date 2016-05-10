eb65668a5345c6daf69d9a6551efc29fbbf31b52
Dominick Peluso <peluso.dominick@gmail.com>
2016-05-10 13:01:26 -0400

    Start browserify build with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,61 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jasmine = require('gulp-jasmine');
 
+
+var browserify = require('browserify');
+var gutil = require('gulp-util');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var sourcemaps = require('gulp-sourcemaps');
+var through = require('through2');
+var globby = require('globby');
+var reactify = require('reactify');
+var uglify = require('gulp-uglify');
+
 // *******************************************
 
+
+gulp.task('build', function(){
+    // gulp expects tasks to return a stream, so we create one here.
+    var bundledStream = through();
+
+    bundledStream
+    // turns the output bundle stream into a stream containing
+    // the normal attributes gulp plugins expect.
+        .pipe(source('app.js'))
+        // the rest of the gulp task, as you would normally write it.
+        // here we're copying from the Browserify + Uglify2 recipe.
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: true}))
+        // Add gulp plugins to the pipeline here.
+        .pipe(uglify())
+        .on('error', gutil.log)
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./dist/js/'))
+        .pipe(connect.reload());
+
+    // "globby" replaces the normal "gulp.src" as Browserify
+    // creates it's own readable stream.
+    globby(['./src/js/*.js']).then(function(entries) {
+        // create the Browserify instance.
+        var b = browserify({
+            entries: entries,
+            debug: true,
+            transform: [reactify]
+        });
+
+        // pipe the Browserify stream into the stream we created earlier
+        // this starts our gulp pipeline.
+        b.bundle().pipe(bundledStream);
+    }).catch(function(err) {
+        // ensure any errors from globby are handled
+        bundledStream.emit('error', err);
+    });
+
+    // finally, we return the stream, so gulp knows when this task is done.
+    return bundledStream;
+
+});
 
 gulp.task('jshint', function(){
     return gulp.src(['src/**/*.js', 'tests/**/*.js'])
