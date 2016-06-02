892a4a2448e47368dba2d733835b618010e2efee
Dominick Peluso <peluso.dominick@gmail.com>
2016-06-02 12:11:38 -0400

    Refactor interface

--- src/js/cmd/preflight.js
+++ src/js/cmd/preflight.js
@@ -1,16 +1,40 @@
+var PreflightFile = require('./../preflightFile');
+
 exports.command = 'preflight [path]';
 
 exports.describe = 'Preflights a single spreadsheet.';
 
 exports.builder = {
     path: {
-        required: true
+        required: true,
+        alias: 'p',
+        describe: "Path to directory."
     },
     output: {
-
+        required: false,
+        alias: 'o',
+        describe: "Output file path."
+    },
+    format: {
+        default: 'html',
+        choices: ['html', 'xml'],
+        alias: 'f',
+        describe: "Preflight report format."
+    },
+    debug: {
+        default: true,
+        choices: [true, false],
+        alias: 'd',
+        describe: "Enable debugging messages to console."
     }
 };
 
 exports.handler = function (argv) {
-    console.log('preflight called for path', argv.dir)
+
+    var preflight = new PreflightFile(argv.path, argv.output, argv.format, argv.debug);
+
+    preflight.init(function(){
+        console.log("Preflight complete.");
+    })
+
 };
